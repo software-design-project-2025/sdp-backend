17450f62b3ce5da228b0f1ce188912e84e23b54a
SNComputerGuru <shillingndivho4@gmail.com>
2025-09-21 21:45:13 +0200

    feat: Add user-specific topic APIs for all, completed and in-progress statuses

--- src/main/java/com/ctrlaltdelinquents/backend/repo/UserRepository.java
+++ src/main/java/com/ctrlaltdelinquents/backend/repo/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, String>{
     List<User> findByUserid(String userid);
 }
