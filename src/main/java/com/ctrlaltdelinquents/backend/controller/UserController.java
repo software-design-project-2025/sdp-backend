a64c0d6bc3b5635d152860421465c8dac1e8a9c7
KwameAmoahh <kwame.amoah498@gmail.com>
2025-09-26 17:16:09 +0200

    Corrected users api route

--- src/main/java/com/ctrlaltdelinquents/backend/controller/UserController.java
+++ src/main/java/com/ctrlaltdelinquents/backend/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
     private final UserRepository userRepository;
 
