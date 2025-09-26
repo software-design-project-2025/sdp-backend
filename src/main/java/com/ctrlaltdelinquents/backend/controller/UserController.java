080463d644ad2339a459aa363d88991fb2c27698
KwameAmoahh <kwame.amoah498@gmail.com>
2025-09-26 14:35:41 +0200

    Adjusted the user controller so the supabaseId can be stored in postgres after creating a new user

--- src/main/java/com/ctrlaltdelinquents/backend/controller/UserController.java
+++ src/main/java/com/ctrlaltdelinquents/backend/controller/UserController.java
@@ -2,15 +2,14 @@ package com.ctrlaltdelinquents.backend.controller;
 
 import com.ctrlaltdelinquents.backend.repo.UserRepository;
 import com.ctrlaltdelinquents.backend.model.User;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.*;
+
+
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/user")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -20,38 +19,16 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("/all")
-    public ResponseEntity<?> getAllUsers() {
-        try {
-            List<User> users = userRepository.findAll();
+    @PostMapping("/createUser")
+    @ResponseBody
+    public User createUser(@RequestBody User user) {
 
-            if (users.isEmpty()) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body("Error: No users found");
-            } else {
-                return ResponseEntity.ok(users);
-            }
-
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Error: Failed to fetch users" + e.getMessage());
+        //Check if user exists
+        List<User> doesUserExist = userRepository.findByUserId(user.getUserid());
+        if (doesUserExist.size() == 0){
+            return userRepository.save(user);
         }
-    }
 
-    @GetMapping("/{userid}")
-    public ResponseEntity<?> getUserById(@PathVariable String userid) {
-        try {
-            List<User> user = userRepository.findByUserid(userid);
-            if(user.isEmpty()) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body("Error: User with id " + userid + "not found");
-            }
-            else{
-                return ResponseEntity.ok(user);
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Error: Failed to fetch user "+ userid+ " by ID" + e.getMessage());
-        }
+        return doesUserExist.get(0);
     }
 }
