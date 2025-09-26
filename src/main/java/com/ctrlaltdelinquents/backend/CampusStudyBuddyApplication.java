35a9a5667cb7d7c8bb9ed60c55f7f4fd7efde52d
KwameAmoahh <kwame.amoah498@gmail.com>
2025-09-26 14:58:02 +0200

    Merged with main

--- src/main/java/com/ctrlaltdelinquents/backend/CampusStudyBuddyApplication.java
+++ src/main/java/com/ctrlaltdelinquents/backend/CampusStudyBuddyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;  // <-- ADD THIS IMPORT
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.ctrlaltdelinquents.backend.repository")
+@EnableJpaRepositories(basePackages = "com.ctrlaltdelinquents.backend.repo")
 public class CampusStudyBuddyApplication {
     public static void main(String[] args) {
         SpringApplication.run(CampusStudyBuddyApplication.class, args);
