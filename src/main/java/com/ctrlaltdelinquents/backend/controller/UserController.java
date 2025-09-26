e8471516ac659398ad930b805bf497d4afe7cc46
KwameAmoahh <135594787+KwameAmoahh@users.noreply.github.com>
2025-09-26 17:25:25 +0200

    Chat page/samuel (#25)
    * Initial commit
    
    * Fixed CORS Registry warning
    
    * Initial commit: api-key system fully operational
    
    * Fixed warning on AuthenticationService page
    
    * chore: Add all backend models corresponding to tables in the database
    
    * chore: Add all models corresponding to tables in the db
    
    * added env to gitignore
    
    * added lombok dependency to build.gradle
    
    * Added lombok dependency in build.gradle
    
    * added chats controller class
    
    * Changed ChatsController to ChatController
    
    * Configured getChatByUser method
    
    * fixed test file merge conflict
    
    * Changed CORS registry to allow requests from Angular
    
    * ChatMessage GET Method works
    
    * Chat-Page functionality added
    
    * Replaced deprecated method in test file
    
    * Adjusted SQL query for model change
    
    * Fixed syntax errors in tests
    
    * Adjusted the user controller so the supabaseId can be stored in postgres after creating a new user
    
    * Merged with main
    
    * Added getUsers
    
    * Corrected users api route
    
    ---------
    
    Co-authored-by: SNComputerGuru <shillingndivho4@gmail.com>
    Co-authored-by: Ndivho Shilling <140017972+SNComputerGuru@users.noreply.github.com>

--- src/main/java/com/ctrlaltdelinquents/backend/controller/UserController.java
+++ src/main/java/com/ctrlaltdelinquents/backend/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
     private final UserRepository userRepository;
 
