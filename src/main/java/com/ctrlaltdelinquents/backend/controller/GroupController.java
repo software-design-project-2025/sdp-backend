c4bd86c07837284e2f82528f7557d81b0e0df4bc
Lesedi <147398294+sedster@users.noreply.github.com>
2025-10-19 13:28:12 +0200

    Sessions page/lesedi (#49)
    * set up sessions endpoints
    
    * set up service
    
    * fixed session creatorid and groupid properties
    
    * created artifacts for group entity
    
    * added deployed site to CORS
    
    * changed int
    
    * minor fixes
    
    ---------
    
    Co-authored-by: monare-code <mc.selahle@gmail.com>

--- src/main/java/com/ctrlaltdelinquents/backend/controller/GroupController.java
+++ src/main/java/com/ctrlaltdelinquents/backend/controller/GroupController.java
@@ -2,23 +2,30 @@ package com.ctrlaltdelinquents.backend.controller;
 
 import com.ctrlaltdelinquents.backend.model.Group;
 import com.ctrlaltdelinquents.backend.model.GroupMembers;
-import com.ctrlaltdelinquents.backend.repo.GroupRepo;
 import com.ctrlaltdelinquents.backend.repo.GroupMembersRepo;
+import com.ctrlaltdelinquents.backend.repo.GroupRepo;
+import com.ctrlaltdelinquents.backend.service.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+//@RequestMapping("/api/groups")
+@CrossOrigin(origins = {"http://localhost:4200", "https://witty-coast-007353203.1.azurestaticapps.net"})
 @RequestMapping("/api/auth/groups")
 public class GroupController {
 
     private final GroupRepo groupRepo;
     private final GroupMembersRepo groupMembersRepo;
+    private GroupService groupService;
 
     public GroupController(GroupRepo groupRepo, GroupMembersRepo groupMembersRepo) {
         this.groupRepo = groupRepo;
         this.groupMembersRepo = groupMembersRepo;
+
     }
 
     @PostMapping
@@ -33,11 +40,17 @@ public class GroupController {
         creatorAsMember.setUserid(savedGroup.getCreatorid());
         groupMembersRepo.save(creatorAsMember);
 
+
         return savedGroup;
     }
 
     @GetMapping("/discover")
     public List<Group> discoverGroups(@RequestParam String userId) {
         return groupRepo.findRandomDiscoverableGroups(userId);
+
     }
+    @GetMapping
+    public ResponseEntity<List<Group>> getAllGroups() {
+        List<Group> groups = groupService.getAllGroups();
+        return ResponseEntity.ok(groups);
 }
